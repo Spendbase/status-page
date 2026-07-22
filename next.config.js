ca652b0f149df4c401699e002775b6a76dc460e7
Dmytro <40298204+dmytrobieliaiev@users.noreply.github.com>
2026-07-22 21:38:39 +0300

    chore: fix dependabot

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const production = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  assetPrefix: production ? '/' : '',
+  output: 'export',
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
